b533336dc8e3d282390de0f28232d5fa191e5e48
Hunt Lin <hunt.lin@tes-tec.com>
2022-11-25 14:16:23 +0800

    Remove spec.

--- spec/features/jasmine_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe 'jasmine', type: :feature, js: true, logged: :admin do
-
-  it 'run tests' do
-    visit "/dummy_entities?jasmine=true"
-    expect(page).to have_css('.jasmine-bar')
-    expect(page.evaluate_script('window.jasmineHelper.parseResult();')).to eq('success')
-  end
-
-end
--- spec/spec_helper.rb
+++
@@ -1,2 +0,0 @@
-require_relative 'init_rails'
-require Rails.root.join('plugins/easyproject/easy_plugins/easy_extensions/test/spec/spec_helper')
